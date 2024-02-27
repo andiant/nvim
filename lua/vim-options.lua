b89a323424546a50fe2b1f18e49e55ab41fb9eb0
Andiant <andi.titar@gmx.de>
2024-02-27 16:51:57 +0100

    create lua config and add lazy-package manager

---
+++ lua/vim-options.lua
@@ -0,0 +1,11 @@
+-- Tab-Config
+vim.cmd("set expandtab")
+vim.cmd("set tabstop=2")
+vim.cmd("set softtabstop=2")
+vim.cmd("set shiftwidth=2")
+vim.cmd("set number")
+vim.cmd("set relativenumber")
+
+vim.g.mapleader = " " -- Leertaste als <leader> Taste, um weitere Befehle auszuführen. Z.B.: <leader>ff
+
+
